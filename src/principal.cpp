56f955df0cfa1019d00bd06eab3d09998db4c2c8
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-11 17:19:35 -0300

    Muestra el título

--- src/principal.cpp
+++ src/principal.cpp
@@ -1,10 +1,14 @@
 
+#include <iostream>
+
 /*
  * Juego del Ahorcado
  *
  * https://es.wikipedia.org/wiki/Ahorcado_(juego)
  */
 int main() {
+
+	std::cout << " A H O R C A D O " << std::endl;
 
 	return 0;
 }
