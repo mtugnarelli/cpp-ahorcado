563982db462f22b6699a0dddad1b1542c3129942
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-11 18:50:34 -0300

    Variable con valor constante

--- src/principal.cpp
+++ src/principal.cpp
@@ -12,7 +12,8 @@ int main() {
 	std::cout << " A H O R C A D O " << std::endl;
 
 	int vidasRestantes = 7;
-	std::string palabraSecreta = "algoritmo";
+	const std::string palabraSecreta = "algoritmo";
+	std::string palabraDescubierta = "---------";
 
 	std::cout << "Te quedan "
 			  << vidasRestantes
