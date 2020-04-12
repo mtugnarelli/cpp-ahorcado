b77fb4fab8edec295cd53bd716eeec9be9e09a00
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-12 15:55:51 -0300

    Declaración previa al uso

--- src/principal.cpp
+++ src/principal.cpp
@@ -12,12 +12,11 @@ int main() {
 	std::cout << " A H O R C A D O " << std::endl;
 
 	int vidasRestantes = 7;
-
-	std::cout << "Te quedan " << vidasRestantes << " vidas" << std::endl;
-
 	std::string palabraSecreta = "algoritmo";
 
-	std::cout << "La palabra secreta tiene "
+	std::cout << "Te quedan "
+			  << vidasRestantes
+			  << " vidas para adivinar una palabra con "
 			  << palabraSecreta.length()
 			  << " letras"
 			  << std::endl;
