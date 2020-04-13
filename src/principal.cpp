b85fad2e63f0618eb5a72df0b20dd7e239837f56
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-04-13 01:18:47 -0300

    Modularizacion (h y cpp)

--- src/principal.cpp
+++ src/principal.cpp
@@ -4,33 +4,10 @@
  * https://es.wikipedia.org/wiki/Ahorcado_(juego)
  */
 
-#include <string>
 #include "interacciones.h"
+#include "reglas.h"
 
 #define VIDAS 7
-
-/*
- * Descubre las ocurrencias de la letra en la palabra 'secreta', actualizando
- * la palabra 'descubierta' y devuelve la cantidad de apariciones.
- */
-int descubrir(char letra, std::string& descubierta, std::string secreta);
-
-/*
- * Según la cantidad de ocurrencias de la letraArriesgada en la
- * palabra secreta consume o no una vida.
- */
-void actualizar(int& vidasRestantes, int ocurrencias, char letraArriesgada);
-
-/*
- * Devuelve si el ahorcado aún no terminó.
- */
-bool sigueJugando(int vidasRestantes, std::string palabraDescubierta,
-				  std::string palabraSecreta);
-
-/*
- * Devuelve una versión oculta de 'palabra'.
- */
-std::string ocultar(std::string palabra);
 
 int main() {
 
@@ -55,47 +32,4 @@ int main() {
 	mostrarResultado(vidasRestantes);
 
 	return 0;
-}
-
-int descubrir(char letra, std::string& descubierta, std::string secreta) {
-
-	int ocurrencias = 0;
-	for (unsigned int i = 0; i < secreta.length(); i++) {
-
-		if (letra == secreta[i]) {
-
-			descubierta[i] = letra;
-			ocurrencias++;
-		}
-	}
-
-	return ocurrencias;
-}
-
-void actualizar(int& vidasRestantes, int ocurrencias, char letraArriesgada) {
-
-	if (ocurrencias == 0) {
-
-		vidasRestantes--;
-
-		mostrarQueNoContiene(letraArriesgada);
-	}
-}
-
-bool sigueJugando(int vidasRestantes, std::string palabraDescubierta,
-				  std::string palabraSecreta) {
-
-	return (vidasRestantes > 0) && (palabraDescubierta != palabraSecreta);
-}
-
-std::string ocultar(std::string palabra) {
-
-	std::string oculta = "";
-
-	for (unsigned int i = 0; i < palabra.length(); i++) {
-
-		oculta = oculta + "-";
-	}
-
-	return oculta;
 }
